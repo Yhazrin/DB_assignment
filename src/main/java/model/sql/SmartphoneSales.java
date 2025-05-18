65d1b05af705f49df04125591de68ee334ed73be
Au-gold-hao <lijinhao214@163.com>
2025-05-18 21:03:43 +0800

    马哥坏

---
+++ src/main/java/model/sql/SmartphoneSales.java
@@ -0,0 +1,71 @@
+package Model;
+
+public class SmartphoneSales {
+    private String country;
+    private String brand;
+    private double column2015;
+    private double column2016;
+    private double column2017;
+    private double column2018;
+    private double column2019;
+    private double column2020;
+    private double column2021;
+    private double column2022;
+    private double column2023;
+    private double column2024;
+
+    // Getters and Setters
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+
+    public String getBrand() { return brand; }
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public double getColumn2015() { return column2015; }
+    public void setColumn2015(double column2015) { this.column2015 = column2015; }
+
+    public double getColumn2016() { return column2016; }
+    public void setColumn2016(double column2016) { this.column2016 = column2016; }
+
+    public double getColumn2017() { return column2017; }
+    public void setColumn2017(double column2017) { this.column2017 = column2017; }
+
+    public double getColumn2018() { return column2018; }
+    public void setColumn2018(double column2018) { this.column2018 = column2018; }
+
+    public double getColumn2019() { return column2019; }
+    public void setColumn2019(double column2019) { this.column2019 = column2019; }
+
+    public double getColumn2020() { return column2020; }
+    public void setColumn2020(double column2020) { this.column2020 = column2020; }
+
+    public double getColumn2021() { return column2021; }
+    public void setColumn2021(double column2021) { this.column2021 = column2021; }
+
+    public double getColumn2022() { return column2022; }
+    public void setColumn2022(double column2022) { this.column2022 = column2022; }
+
+    public double getColumn2023() { return column2023; }
+    public void setColumn2023(double column2023) { this.column2023 = column2023; }
+
+    public double getColumn2024() { return column2024; }
+    public void setColumn2024(double column2024) { this.column2024 = column2024; }
+
+    @Override
+    public String toString() {
+        return "SmartphoneSales{" +
+                "country='" + country + '\'' +
+                ", brand='" + brand + '\'' +
+                ", column2015='" + column2015 + '\'' +
+                ", column2016=" + column2016 +
+                ", column2017=" + column2017 +
+                ", column2018=" + column2018 +
+                ", column2019=" + column2019 +
+                ", column2020=" + column2020 +
+                ", column2021=" + column2021 +
+                ", column2022=" + column2022 +
+                ", column2023=" + column2023 +
+                ", column2024=" + column2024 +
+                '}';
+    }
+}
